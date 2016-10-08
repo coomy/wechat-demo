b07cb421e8264fb06344396f3617a471bd373804
gaoming <gaoming@gaomingdeMacBook-Pro.local>
2016-10-09 00:33:41 +0800

    modify the line end ecoding problem.

--- preProcess.sh
+++ preProcess.sh
@@ -1,2 +1,3 @@
-wx\wcc app_src/pages/index/index.wxml -o pages/index/index_vdom.js
-wx\wcc app_src/pages/detail/detail.wxml -o pages/detail/detail_vdom.js
+#!/bin/bash
+./wx/wcc app_src/pages/index/index.wxml -o pages/index/index_vdom.js
+./wx/wcc app_src/pages/detail/detail.wxml -o pages/detail/detail_vdom.js
